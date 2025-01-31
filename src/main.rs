6dc8c92b1e77bd5cf92b20d8745103682806ee54
jagoum <jagoum24@gmail.com>
2025-01-31 05:51:05 +0100

    Modified main function

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,43 @@
-use std::{arch::x86_64::_XCR_XFEATURE_ENABLED_MASK, env::{self, args}, fmt::Write};
+use std::env;
+
 use adding::add;
-use factorial::fact;
-use subtracting::sub;
-use multiply::mult;
 use divide::div;
+use factorial::fact;
+use modulus::modulus;
+use multiply::mult;
+use subtracting::sub;
 mod divide;
-mod multiply;
 mod factorial;
 pub mod modulus;
+mod multiply;
 
 fn main() {
-let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-let len = args.len();
+    let _len = args.len();
 
-let x = &args[1];
-let opt=&args[2];
-let opt:char = opt.trim().parse().expect("Error");
-let y =&args[3];
+    let x = &args[1];
+    let opt = &args[2];
+    let opt: char = opt.trim().parse().expect("Error");
+    let y = &args[3];
 
-let x: f64 = x.trim().parse().expect("Enter a number");
-let y: f64 = y.trim().parse().expect("Enter a number");
-if len == 4 {
-match opt
- {
-    '+' => println!("Result of {x} + {y} = {}",add(x,y)), 
-    '-' => println!("Result of {x} - {y} = {}",sub(x,y)),
-    'x' => println!("Result of {x} x {y} = {}",mult(x,y)),
-    '/' => println!("Result of {x} / {y} = {}",div(x, y)) ,
-    _ => println!("Please Enter either + ,  -, *, /") 
-};
+    let x: f64 = x.trim().parse().expect("Enter a number");
+    let y: f64 = y.trim().parse().expect("Enter a number");
+
+
+  
+    match opt {
+        '+' => println!("Result of {x} + {y} = {}", add(x, y)),
+        '-' => println!("Result of {x} - {y} = {}", sub(x, y)),
+        'x' => println!("Result of {x} x {y} = {}", mult(x, y)),
+        '/' => println!("Result of {x} / {y} = {}", div(x, y)),
+        '%' => println!("Result of {x} % {y} = {}", modulus(x as u128, y as u128)),
+        '!' => println!("Result of {}! = {}", x, fact(x as u128, x as u128)),
+
+        _ => println!("Please Enter either +,  -, x, /, %"),
+    };
+
 }
 
-}
-
-mod subtracting;
 mod adding;
+mod subtracting;
